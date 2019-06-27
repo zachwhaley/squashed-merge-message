e6200f5c66c8d61444898c1f0aecfce95d0cd86b
Zachary Whaley (RD-US) <Zachary_Whaley@trendmicro.com>
2019-06-27 16:55:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Squashed Merge Message
+
+Use Pull Request description as Squash and Merge commit message
---
+++ content.js
@@ -0,0 +1,17 @@
+var squashMergeMessage = function() {
+  var squashButton = document.querySelector('.merge-message .btn-group-squash [type=submit]');
+  if (!squashButton) return;
+
+  squashButton.addEventListener('click', function() {
+    var description = document.querySelector('.comment-form-textarea[name=\'pull_request[body]\']');
+    if (!description) return;
+
+    var messageField = document.getElementById('merge_message_field');
+    if (!messageField) return;
+
+    messageField.value = description.textContent;
+  });
+}
+
+document.addEventListener('pjax:end', squashMergeMessage);
+squashMergeMessage();
