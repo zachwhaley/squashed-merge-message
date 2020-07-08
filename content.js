8f73dc2387389e817639d2cf147e70c4c0d42bba
Geoffrey Martin-Noble <gcmn@google.com>
2020-07-08 12:02:48 -0700

    Add support for merge commits

--- content.js
+++ content.js
@@ -1,18 +1,38 @@
-var squashMergeMessage = function() {
-  var squashButton = document.querySelector('.merge-message .btn-group-squash');
-  
-  if (!squashButton) return;
+var copyPrDescription = function() {
+  var prTitleEl = document.getElementById("issue_title");
+  if (!prTitleEl) return;
 
-  squashButton.addEventListener('click', function() {
-    var description = document.querySelector('.comment-form-textarea[name="pull_request[body]"]');
-    if (!description) return;
+  var prNumberEl = document.querySelector(".gh-header-title .gh-header-number");
+  if (!prNumberEl) return;
 
-    var messageField = document.getElementById('merge_message_field');
-    if (!messageField) return;
+  var prBodyEl = document.querySelector('.comment-form-textarea[name="pull_request[body]"]');
+  if (!prBodyEl) return;
 
-    messageField.value = description.textContent;
-  });
+  var titleField = document.getElementById('merge_title_field');
+  if (!titleField) return;
+
+  var messageField = document.getElementById('merge_message_field');
+  if (!messageField) return;
+
+  var commitTitle = `${prTitleEl.value} (${prNumberEl.textContent})`;
+  var commitBody = prBodyEl.textContent;
+
+  titleField.value = commitTitle;
+  messageField.value = commitBody;
 }
 
-document.addEventListener('pjax:end', squashMergeMessage);
-squashMergeMessage();
+var addMergeListeners = function() {
+  var squashButton = document.querySelector('.merge-message .btn-group-squash');
+  var mergeButton = document.querySelector('.merge-message .btn-group-merge');
+
+  if (squashButton) {
+    squashButton.addEventListener('click', copyPrDescription);
+  }
+  if (mergeButton) {
+    mergeButton.addEventListener('click', copyPrDescription);
+  }
+
+}
+
+document.addEventListener('pjax:end', addMergeListeners);
+addMergeListeners();
