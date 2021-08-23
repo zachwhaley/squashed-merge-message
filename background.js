2a718b2a41370867612c650a11c6f3be4b796939
Zachary Whaley <zachbwhaley@gmail.com>
2021-08-23 15:23:30 -0500

    Migrate to manifest version 3

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+import 'webext-dynamic-content-scripts';
+import addDomainPermissionToggle from 'webext-domain-permission-toggle';
+
 // adds the "Enable Squashed Merge Message on this domain" menu item
 // so the user can enable the extension on custom Github Enterprise domains
 addDomainPermissionToggle();
